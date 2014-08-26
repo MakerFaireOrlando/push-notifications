26629c0386fa77086dff41d9dd6d31009d3347e5
Cory Kinberger <coryk135@gmail.com>
2014-08-26 00:28:39 -0400

    Clean up code a bit and add some comments. iOS should be completely supported but needs testing.

--- app.js
+++ app.js
@@ -6,18 +6,20 @@
         iOSClientIDsSetName = 'iOSClientIDs',
         apnConnection = new apn.Connection({});
 
+        // clientIDsMap is used as a HashMap to see if the
+        // clientIDs array contains a specific ID
     var clientIDsMap = {},
         clientIDs = [],
-        iOSClientIDsMap = {"f44182de 1688fcd9 27b87925 547b5035 0bfbbc83 2d415c1e a5e48a59 3616ef57":true},
-        iOSClientIDs = ["f44182de 1688fcd9 27b87925 547b5035 0bfbbc83 2d415c1e a5e48a59 3616ef57"];
-
+        iOSClientIDsMap = {},
+        iOSClientIDs = [];
     redisClient.on('error', function(err) {
       console.log('Redis Error: ' + err);
     });
 
     redisClient.on('ready', function(){
-      // smembers = set members. Returns all of the elements of a set.
-      redisClient.smembers('clientIDs', function(err, reply){
+      // Get the set of android device IDs from redis, iterate over them all
+      // and add them to the clientIDsMap object and the clientIDs array
+      redisClient.smembers(clientIDsSetName, function(err, reply){
         if(err){
           console.log("Error: " + err);
         } else {
@@ -29,9 +31,23 @@
             }
           }
         }
-      })
-    })
+      });
+      redisClient.smembers(iOSClientIDsSetName, function(err, reply){
+        if(err){
+          console.log("Error: " + err);
+        } else {
+          console.log(reply);
+          for(var i = 0; i < reply.length; i ++){
+            if(!clientIDsMap[reply[i]]){
+              clientIDsMap[reply[i]] = true;
+              clientIDs.push(reply[i]);
+            }
+          }
+        }
+      });
+    });
 
+    // Helper function for GCM
     function newMessage(data) {
         return new gcm.Message({
             collapseKey: 'demo',
@@ -73,9 +89,12 @@
     app.use(bodyParser());
 
     /*
-     * i forgot what i was going to write here... 
+     * Routes
      */
     app.route('/register/android')
+        /**
+         * POST /register/android {"id": "the_device_id"}
+         */
         .post(function(req, res, next){
             var body = req.body,
                 data = body.data,
@@ -85,6 +104,9 @@
             res.send(200);
         });
     app.route('/register/ios')
+        /**
+         * POST /register/ios {"id": "the_device_id"}
+         */
         .post(function(req, res, next){
             var body = req.body,
                 data = body.data,
@@ -94,7 +116,12 @@
             res.send(200);
         });
     app.route('/broadcast/:text')
-      .get(function(req, res, next){
+        /**
+         * TODO: This is only a temporary way to send push notifications.
+         *       We need to add some form of authentication.
+         * GET /broadcast/:text
+         */
+        .get(function(req, res, next){
             var text = req.params.text;
             var message = newMessage({message:text}); 
   
